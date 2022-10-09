cca8a6e366555f23bff3e8f8cdc05ff53a3353f3
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2022-10-09 11:41:13 +0100

    implement order business logic 01

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,12 +2,12 @@ package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
+
+import tacos.data.OrderRepository;
 import tacos.model.TacoOrder;
 
 import javax.validation.Valid;
@@ -17,6 +17,12 @@ import javax.validation.Valid;
 @Slf4j
 @SessionAttributes("tacoOrder")
 public class OrderController {
+
+    private OrderRepository repo;
+
+    public OrderController(OrderRepository repo) {
+        this.repo = repo;
+    }
 
     @GetMapping("current")
     public String showOrder() {
@@ -28,8 +34,17 @@ public class OrderController {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+        var order = repo.save(tacoOrder);
         log.info("Order submitted - ", tacoOrder);
         sessionStatus.setComplete();
-        return "redirect:/design";
+        return "redirect:/orders?id="+order.getId();
     }
+
+    @GetMapping()
+    public String orderDetail(@RequestParam Long id, Model model) {
+        TacoOrder order = repo.findById(id).orElseThrow();
+        model.addAttribute("order",order);
+        return "orderInfo";
+    }
+
 }
