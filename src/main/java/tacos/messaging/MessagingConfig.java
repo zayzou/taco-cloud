c4a44a681db72eee9e2e12324e1d7ce2eb7b3385
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-03-22 10:23:45 +0100

    MessagingConfig for artemis

---
+++ src/main/java/tacos/messaging/MessagingConfig.java
@@ -0,0 +1,35 @@
+package tacos.messaging;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import tacos.model.Taco;
+
+import javax.jms.Destination;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class MessagingConfig {
+
+
+    @Bean
+    public MappingJackson2MessageConverter messageConverter() {
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+        messageConverter.setTypeIdPropertyName("_typeId");
+
+        Map<String, Class<?>> typeIdMappings = new HashMap<>();
+        typeIdMappings.put("taco", Taco.class);
+        messageConverter.setTypeIdMappings(typeIdMappings);
+
+        return messageConverter;
+
+    }
+
+    @Bean
+    public Destination tacoQueue() {
+        return new ActiveMQQueue("tacocloud.taco.queue");
+    }
+
+}
