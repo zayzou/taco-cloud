1fabf396fb481d9e85d0eb4c58042065e242d945
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-03-22 10:24:37 +0100

    Add specified property to message,add injected queue

--- src/main/java/tacos/messaging/JmsTacoMessagingService.java
+++ src/main/java/tacos/messaging/JmsTacoMessagingService.java
@@ -4,19 +4,30 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import tacos.model.Taco;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+
 @Service
 public class JmsTacoMessagingService implements TacoMessagingService {
 
     private JmsTemplate jms;
+    private Destination tacoQueue;
 
-    public JmsTacoMessagingService(JmsTemplate jms) {
+    public JmsTacoMessagingService(JmsTemplate jms, Destination tacoQueue) {
         this.jms = jms;
+        this.tacoQueue = tacoQueue;
     }
 
 
     @Override
     public void sendTaco(Taco taco) {
-        jms.convertAndSend(taco);
+        jms.convertAndSend(tacoQueue, taco, this::addTacoSource);
 //        jms.send(session -> session.createObjectMessage(taco));
+    }
+
+    private Message addTacoSource(Message message) throws JMSException {
+        message.setStringProperty("X_TACO_SOURCE", "WEB");
+        return message;
     }
 }
